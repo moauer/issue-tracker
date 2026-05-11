729353eb8072ac8d06c2909318d00b671d545c5a
moauer <mona.auer@gmx.at>
2026-05-11 14:24:35 +0200

    code

--- app.py
+++ app.py
@@ -3,3 +3,7 @@ Issue Tracker App Code File
 Author: Auer Mona
 Date: April 27 2024
 """
+import streamlit as st
+import datetime as date
+
+st.set_page_config(page_title="Issue Tracker", page_icon=":memo:", layout="wide")
